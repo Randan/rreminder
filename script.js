8a3e309ccb7b53c7bfa313333b8d3d988f626e5b
Andrew Randan <andrew.randan@mabbly.com>
2019-05-07 01:57:46 +0300

    data from json

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-const reminders = [
-    {
-        name: "breakfast",
-        time: {
-            hours: 21,
-            minutes: 13
-        },
-        week: [1, 2, 3, 4, 5],
-        message: "Go and have a breakfast",
-        wasReminded: false
-    },
-    {
-        name: "lunch",
-        time: {
-            hours: 21,
-            minutes: 14
-        },
-        week: [1, 2, 3, 4, 5],
-        message: "Go and have a lunch",
-        wasReminded: false
-    },
-    {
-        name: "dinner",
-        time: {
-            hours: 21,
-            minutes: 15
-        },
-        week: [1, 2, 3, 4, 5],
-        message: "Go and have a dinner",
-        wasReminded: false
-    }
-];
-
-// console.log(reminders);
-
 function timer() {
     const date = new Date();
     const day = date.getDay();
@@ -41,37 +6,56 @@ function timer() {
     const seconds = date.getSeconds(); // TODO: remove seconds
 
     return { day, hours, minutes, seconds }; // TODO: remove seconds
-}
+};
 
-// let wasReminded = false;
+function getData() {
+    const requestURL = './db.json';
+    const request = new XMLHttpRequest();
 
-function reminder() {
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        const data = request.response;
+        init(data);
+    }
+};
+
+function reminder(data) {
     const time = timer();
-    // console.log(time);
     const { day, hours, minutes, seconds } = time; // TODO: remove seconds
+    const { reminders } = data;
+
     $('#timer').text(`${hours}:${minutes}:${seconds}`);
-    
     $('.item').remove();
 
     reminders.forEach(reminder => {
-
-        // console.log(reminder.name, reminder.wasReminded);
-        $('#reminders').append(`<div class="item">time: ${reminder.time.hours}:${reminder.time.minutes} - "${reminder.message}" - was reminded: ${reminder.wasReminded}</div>`);
-
         reminder.wasReminded === false
-            ? (
-                day < 6 && hours === reminder.hours && minutes === reminder.minutes
-                    && (reminder.wasReminded = true)
-                    && console.log(reminder.name, reminder.wasReminded)
-                    // && alert(reminder.message) // TODO: uncomment
-            )
-            : (
-                minutes === reminder.minutes + 1
-                    && (reminder.wasReminded = false)
-                    && console.log(reminder.name, reminder.wasReminded)
-            );
+        ? (
+            day < 6 && hours === reminder.hours && minutes === reminder.minutes
+                && (reminder.wasReminded = true)
+                && console.log(reminder.name, reminder.wasReminded)
+                // && alert(reminder.message) // TODO: uncomment
+        )
+        : (
+            minutes === reminder.minutes + 1
+                && (reminder.wasReminded = false)
+                && console.log(reminder.name, reminder.wasReminded)
+        );
+
+        $('#reminders')
+            .append(`
+                <div class="item">
+                    time: ${reminder.time.hours}:${reminder.time.minutes} /// ${reminder.name} /// was reminded: ${reminder.wasReminded}
+                </div>
+            `);
     });
 
-}
+};
 
-setInterval(reminder, 1000);
+function init(data) {
+    setInterval(() => reminder(data), 1000);
+};
+
+// Init
+getData();
